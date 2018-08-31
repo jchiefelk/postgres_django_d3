2393a23352947bd6528763ef87befc5704b6c37a
Jackson Chief Elk <jackson.chiefelk@gmail.com>
2018-08-30 18:32:56 -0700

    Add Client Model and make migration

--- restapi/models.py
+++ restapi/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Clients(models.Model):
+	AccountDescriptiveName = models.CharField(max_length=200)
+	CampaignId = models.IntegerField()
+	CampaignName = models.CharField(max_length=200)
+	CampaignStatus = models.CharField(max_length=200)
+	CityCriteriaId = models.IntegerField()
+	CountryCriteriaId = models.IntegerField()
+	CustomerDescriptiveName = models.CharField(max_length=200)
+	ExternalCustomerId = models.IntegerField()
+	IsTargetingLocation = models.BooleanField(default=True)
+	MetroCriteriaId = models.CharField(max_length=200)
+	MostSpecificCriteriaId = models.IntegerField()
+	RegionCriteriaId = models.IntegerField()
+	Date = models.DateField()
+	Device = models.CharField(max_length=200)
+	LocationType = models.CharField(max_length=200)
+	AveragePosition = models.IntegerField
+	Clicks = models.IntegerField()
+	Conversions = models.FloatField()
+	ConversionValue = models.FloatField()
+	Cost = models.IntegerField()
+	Impressions = models.IntegerField()
+	Interactions = models.IntegerField()
+	InteractionTypes = models.CharField(max_length=200)
+	VideoViews = models.IntegerField()
