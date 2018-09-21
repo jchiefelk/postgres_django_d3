8323fbc6e55f443abaae68c7e9e8c4175d3eab44
Jackson Chief Elk <jackson.chiefelk@gmail.com>
2018-09-21 10:39:23 -0600

    Remove depencies and responses for get requests

--- restapi/views.py
+++ restapi/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from restapi.models import Clients
+from rest_framework.decorators import api_view
+
+@api_view(['GET', 'POST'])
+def snippet_list(request):
+  if request.method == 'GET' and request.query_params.get('date', None)==None:
+    return JsonResponse({"models_to_return": list(Clients.objects.values())})
+  else: 
+    return JsonResponse({"data": list(Clients.objects.filter(date = request.query_params.get('date', None)).values())})
 
 # Create your views here.
 def index(request):
