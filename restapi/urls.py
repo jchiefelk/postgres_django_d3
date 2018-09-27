65c7e4136093fb80d624cd6a2f94648cb8d24f66
Jackson Chief Elk <jackson.chiefelk@gmail.com>
2018-09-26 19:44:32 -0700

    Add render static html template on client side

--- restapi/urls.py
+++ restapi/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
-
-from . import views
+from django.views import generic
 
 urlpatterns = [
-	url(r'^$',views.index,name='index'),
+	url(r'^', generic.TemplateView.as_view(template_name='index.html'))
 ]
