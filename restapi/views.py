f4252d6f65c7191e2a7838dec1191455fac1b7cd
Jackson Chief Elk <jackson.chiefelk@gmail.com>
2018-09-26 19:43:51 -0700

    Change how requests are handled for the api

--- restapi/views.py
+++ restapi/views.py
@@ -1,14 +1,11 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from restapi.models import Clients
 from rest_framework.decorators import api_view
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 def snippet_list(request):
-  if request.method == 'GET' and request.query_params.get('date', None)==None:
+  if request.method == 'GET' and request.query_params.get('date', None)== None:
     return JsonResponse({"models_to_return": list(Clients.objects.values())})
-  else: 
-    return JsonResponse({"data": list(Clients.objects.filter(date = request.query_params.get('date', None)).values())})
 
-# Create your views here.
-def index(request):
-	return HttpResponse("RESTAPI View")
+  if request.method == 'GET' and request.query_params.get('date', None) != None: 	
+    return JsonResponse({"data": list(Clients.objects.filter(date=request.query_params.get('date', None)).values())})
