117af05f5f453ee1a9fe2e2ab69f8c1a48f82312
Jackson Chief Elk <jackson.chiefelk@gmail.com>
2018-10-15 11:06:28 -0700

    Add restful routes that serve avg values

--- restapi/views.py
+++ restapi/views.py
@@ -1,11 +1,40 @@
 from django.http import JsonResponse
 from restapi.models import Clients
 from rest_framework.decorators import api_view
+from django.db.models import Avg
 
 @api_view(['GET'])
 def snippet_list(request):
-  if request.method == 'GET' and request.query_params.get('date', None)== None:
-    return JsonResponse({"models_to_return": list(Clients.objects.values())})
+  if request.method == 'GET' and request.query_params.get('date', None) == None:
 
-  if request.method == 'GET' and request.query_params.get('date', None) != None: 	
-    return JsonResponse({"data": list(Clients.objects.filter(date=request.query_params.get('date', None)).values())})
+    compvalue = Clients.objects.filter(device='Computers').values('conversion_value').aggregate(Avg('conversion_value'))
+
+    tabvalue = Clients.objects.filter(device='Tablets with full browsers').aggregate(Avg('conversion_value'))
+
+    mobilevalue = Clients.objects.filter(device='Mobile devices with full browsers').aggregate(Avg('conversion_value'))
+
+    response = {
+      "date": "all",
+      "computers": compvalue,
+      "tablets": tabvalue,
+      "mobile": mobilevalue
+    }
+
+  if request.method == 'GET' and request.query_params.get('date', None) != None:
+    
+    compvalue = Clients.objects.filter(device='Computers',date=request.query_params.get('date', None)).aggregate(Avg('conversion_value'))
+
+    tabvalue = Clients.objects.filter(device='Tablets with full browsers',date=request.query_params.get('date', None)).aggregate(Avg('conversion_value'))
+
+    mobilevalue = Clients.objects.filter(device='Mobile devices with full browsers',date=request.query_params.get('date', None)).aggregate(Avg('conversion_value'))
+
+    response = {
+      "date": request.query_params.get('date', None),
+      "computers": compvalue,
+      "tablets": tabvalue,
+      "mobile": mobilevalue
+    }
+
+  return JsonResponse({
+    "data": response 
+  })
